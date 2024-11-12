c1d7cbe2c74969ba82066416c6898fcb9455710f
Bianca Ribeiro Mazotti <biancaribeiromazotti@gmail.com>
2024-11-12 18:39:24 -0300

    feature/Criacao do projeto comercium

---
+++ protected/config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+    'connectionString' => 'pgsql:host=localhost;port=5432;dbname=comercium',
+    'username' => 'postgres',
+    'password' => 'postgres',
+    'charset' => 'utf8',
+);
